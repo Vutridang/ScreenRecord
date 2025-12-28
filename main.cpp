c088b1dd0a9732f4dee6d2c832a4b21bdd1d84d2
Vutridang <dangvu8244@gmail.com>
2025-12-28 12:58:40 +0700

    feat: add basic UI layout with title and record button - add vertical layout to main widget - add title label and record button - apply basic styles to UI elements

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #include <QApplication>
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +12,18 @@ int main(int argc, char *argv[])
     QWidget mainWindow;
     mainWindow.setWindowTitle("Screen Recorder");
     mainWindow.resize(300, 100);
+
+    QVBoxLayout *layout = new QVBoxLayout(&mainWindow);
+
+    QLabel *titleLabel = new QLabel("Screen Recorder");
+    titleLabel->setAlignment(Qt::AlignCenter);
+    titleLabel->setStyleSheet("font-size: 22px; color: blue;");
+
+    layout->addWidget(titleLabel);
+
+    QPushButton *recordButton = new QPushButton("Record");
+    recordButton->setStyleSheet("font-size: 16px; color: white; background-color: green");
+    layout->addWidget(recordButton);
 
     mainWindow.show();
 
